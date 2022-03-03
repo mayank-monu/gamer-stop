6ca17e9f1a04f0cba33a82091e65f26655a53544
Mayank Monu <mayankmono29@gmail.com>
2022-03-03 14:23:20 +0530

    Routing and home page done

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './menu-item.styles.scss';
+import { withRouter } from "react-router-dom";
 
-
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className = {`${size} menu-item`}>
+//withRouter is a Higher Order Component that takes a normal component and return a super component
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div className = {`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className= 'background-image'
             style = {{
                 backgroundImage : `url(${imageUrl})`
@@ -16,5 +18,5 @@ const MenuItem = ({title, imageUrl, size}) => (
         </div>
     </div>
 );
-
-export default MenuItem;
+//size in div above customises our different product section's size
+export default withRouter(MenuItem);
